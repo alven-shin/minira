0f13f50061513c4af60b996b9c24deec6595c08c
Alvin Shen <29553973+alven-shin@users.noreply.github.com>
2025-01-03 21:28:39 -0500

    feat: keep track of file changes

---
+++ src/file_sync.rs
@@ -0,0 +1,43 @@
+use crate::Backend;
+use tower_lsp::lsp_types::*;
+
+pub async fn handle_did_open(
+    backend: &Backend,
+    DidOpenTextDocumentParams {
+        text_document: TextDocumentItem { uri, text, .. },
+    }: DidOpenTextDocumentParams,
+) {
+    backend.opened_files.insert(uri, text);
+}
+
+pub async fn handle_did_close(backend: &Backend, params: DidCloseTextDocumentParams) {
+    backend.opened_files.remove(&params.text_document.uri);
+}
+
+pub async fn handle_did_change(backend: &Backend, mut params: DidChangeTextDocumentParams) {
+    let [TextDocumentContentChangeEvent {
+        range: None,
+        range_length: None,
+        ref mut text,
+    }] = &mut params.content_changes[..]
+    else {
+        backend
+            .client
+            .log_message(
+                MessageType::ERROR,
+                "expected single change containing entire document",
+            )
+            .await;
+        return;
+    };
+
+    let Some(mut file) = backend.opened_files.get_mut(&params.text_document.uri) else {
+        backend
+            .client
+            .log_message(MessageType::WARNING, "document not open")
+            .await;
+        return;
+    };
+
+    std::mem::swap(file.value_mut(), text);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,23 @@
+use dashmap::DashMap;
 use tower_lsp::jsonrpc::Result;
 use tower_lsp::lsp_types::*;
 use tower_lsp::{Client, LanguageServer, LspService, Server};
 
+mod file_sync;
+
 #[derive(Debug)]
 struct Backend {
     client: Client,
+    opened_files: DashMap<Url, String>,
+}
+
+impl Backend {
+    fn with_client(client: Client) -> Self {
+        Self {
+            client,
+            opened_files: DashMap::new(),
+        }
+    }
 }
 
 #[tower_lsp::async_trait]
@@ -15,7 +28,18 @@ impl LanguageServer for Backend {
                 name: env!("CARGO_PKG_NAME").to_string(),
                 version: Some(env!("CARGO_PKG_VERSION").to_string()),
             }),
-            ..Default::default()
+            capabilities: ServerCapabilities {
+                text_document_sync: Some(TextDocumentSyncCapability::Options(
+                    TextDocumentSyncOptions {
+                        open_close: Some(true),
+                        change: Some(TextDocumentSyncKind::FULL),
+                        will_save: Some(false),
+                        will_save_wait_until: Some(false),
+                        save: Some(TextDocumentSyncSaveOptions::Supported(false)),
+                    },
+                )),
+                ..Default::default()
+            },
         })
     }
 
@@ -28,6 +52,18 @@ impl LanguageServer for Backend {
             .await;
     }
 
+    async fn did_open(&self, params: DidOpenTextDocumentParams) {
+        file_sync::handle_did_open(self, params).await;
+    }
+
+    async fn did_close(&self, params: DidCloseTextDocumentParams) {
+        file_sync::handle_did_close(self, params).await;
+    }
+
+    async fn did_change(&self, params: DidChangeTextDocumentParams) {
+        file_sync::handle_did_change(self, params).await;
+    }
+
     async fn shutdown(&self) -> Result<()> {
         Ok(())
     }
@@ -37,6 +73,6 @@ pub async fn run() {
     let stdin = tokio::io::stdin();
     let stdout = tokio::io::stdout();
 
-    let (service, socket) = LspService::new(|client| Backend { client });
+    let (service, socket) = LspService::new(Backend::with_client);
     Server::new(stdin, stdout, socket).serve(service).await;
 }
