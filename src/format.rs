3eff4313c656c9a41f3edbfb828cc4aa412faf4b
Alvin Shen <29553973+alven-shin@users.noreply.github.com>
2025-01-06 18:15:46 -0500

    refactor: produce diff for formatting instead of replacing the entire document

--- src/format.rs
+++ src/format.rs
@@ -1,5 +1,6 @@
 use std::process::Stdio;
 
+use similar::{DiffOp, TextDiff};
 use tokio::io::AsyncWriteExt as _;
 use tokio::process::Command;
 use tower_lsp::jsonrpc::Result;
@@ -8,6 +9,7 @@ use tower_lsp::lsp_types::*;
 use crate::error::{self, FILE_NOT_OPEN};
 use crate::Backend;
 
+#[expect(clippy::too_many_lines)]
 pub async fn handle_formatting(
     backend: &Backend,
     params: DocumentFormattingParams,
@@ -28,38 +30,139 @@ pub async fn handle_formatting(
     // write the contents to rustfmt's stdin
     let mut stdin = child.stdin.take().expect("failed to open stdin");
     let write_stdin = async {
-        for chunk in document.value().chunks() {
-            stdin
-                .write_all(chunk.as_bytes())
-                .await
-                .expect("failed to write to stdin");
-        }
-        drop(stdin);
+        let original = document.to_string();
+        stdin
+            .write_all(original.as_bytes())
+            .await
+            .expect("failed to write to stdin");
+        drop(stdin); // ensure rustfmt receives EOF
+        original
     };
 
     // wait for rustfmt to finish
-    let ((), output) = tokio::join!(write_stdin, child.wait_with_output());
+    let (original, output) = tokio::join!(write_stdin, child.wait_with_output());
     let output = output.expect("failed to wait on rustfmt");
 
     if !output.status.success() {
         return Err(error::rustfmt_failed(output.status));
     }
 
+    // diff the original and formatted text
     let new_text = String::from_utf8(output.stdout).expect("rustfmt output was not valid utf-8");
-    let lines = document.len_lines() - 1;
+    let mut edits = Vec::new();
+    let diff = TextDiff::from_lines(&original, &new_text);
 
-    Ok(Some(vec![TextEdit {
-        range: Range {
-            start: Position {
-                line: 0,
-                character: 0,
-            },
-            end: Position {
-                #[expect(clippy::cast_possible_truncation)]
-                line: lines as _,
-                character: 0,
-            },
-        },
-        new_text,
-    }]))
+    // convert the diff to text edits
+    for op in diff.ops() {
+        match op {
+            DiffOp::Equal { .. } => (),
+            DiffOp::Delete {
+                old_index, old_len, ..
+            } => {
+                edits.push(TextEdit {
+                    range: Range {
+                        #[expect(clippy::cast_possible_truncation)]
+                        start: Position {
+                            line: *old_index as _,
+                            character: 0,
+                        },
+                        #[expect(clippy::cast_possible_truncation)]
+                        end: Position {
+                            line: (old_index + old_len) as _,
+                            character: 0,
+                        },
+                    },
+                    new_text: String::new(),
+                });
+            }
+            DiffOp::Insert {
+                old_index,
+                new_index,
+                new_len,
+            } => {
+                edits.push(TextEdit {
+                    range: Range {
+                        #[expect(clippy::cast_possible_truncation)]
+                        start: Position {
+                            line: *old_index as _,
+                            character: 0,
+                        },
+                        #[expect(clippy::cast_possible_truncation)]
+                        end: Position {
+                            line: *(old_index) as _,
+                            character: 0,
+                        },
+                    },
+                    new_text: InterspersedLines::from(
+                        new_text.lines().skip(*new_index).take(*new_len),
+                    )
+                    .collect(),
+                });
+            }
+            DiffOp::Replace {
+                old_index,
+                old_len,
+                new_index,
+                new_len,
+            } => {
+                edits.push(TextEdit {
+                    range: Range {
+                        #[expect(clippy::cast_possible_truncation)]
+                        start: Position {
+                            line: *old_index as _,
+                            character: 0,
+                        },
+                        #[expect(clippy::cast_possible_truncation)]
+                        end: Position {
+                            line: (old_index + old_len) as _,
+                            character: 0,
+                        },
+                    },
+                    new_text: InterspersedLines::from(
+                        new_text.lines().skip(*new_index).take(*new_len),
+                    )
+                    .collect(),
+                });
+            }
+        }
+    }
+
+    Ok(Some(edits))
+}
+
+/// custom iterator to reinsert the removed newlines from Lines iterator
+struct InterspersedLines<T> {
+    iter: T,
+    newline: bool,
+}
+
+impl<'a, T> From<T> for InterspersedLines<T>
+where
+    T: Iterator<Item = &'a str>,
+{
+    fn from(iter: T) -> Self {
+        Self {
+            iter,
+            newline: false,
+        }
+    }
+}
+
+impl<'a, T> Iterator for InterspersedLines<T>
+where
+    T: Iterator<Item = &'a str>,
+{
+    type Item = &'a str;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.newline {
+            self.newline = false;
+            Some("\n")
+        } else if let x @ Some(_) = self.iter.next() {
+            self.newline = true;
+            x
+        } else {
+            None
+        }
+    }
 }
